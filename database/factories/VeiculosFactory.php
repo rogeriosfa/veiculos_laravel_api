0885e890bb316bf3abe7a1a097a46f7b03ca86e7
Rogério Rodrigues <rogeriosfa@gmail.com>
2018-07-03 17:31:52 -0300

    migrate

---
+++ database/factories/VeiculosFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Veiculos::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2018_07_03_185811_create_veiculos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVeiculosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    // public function up()
+    // {
+    //     Schema::create('veiculos', function (Blueprint $table) {
+    //         $table->increments('id');
+    //         $table->string('veiculo');
+    //         $table->string('marca');
+    //         $table->integer('ano');
+    //         $table->text('descricao');
+    //         $table->boolean('vendido');
+    //         //$table->timestamps('created');
+    //         //$table->timestamps('updated');
+    //     });
+    // }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    // public function down()
+    // {
+    //     Schema::dropIfExists('veiculos');
+    // }
+}
